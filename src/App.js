b8f7009f027024067d735d0f5ebf89c00aa5c1ac
VasileiosVal <vallasvasileios@hotmail.com>
2019-12-23 13:54:11 +0200

    using observable streams and pipe chaining in order to fetch data from a backend service and manipulate them accordingly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { interval } from "rxjs";
+import { Observable, interval, noop } from "rxjs";
+import { map } from "rxjs/operators";
+import axios from "axios";
 
 const App = () => {
   useEffect(() => {
-    const first$ = interval(1000);
-    first$.subscribe(res => {
-      console.log(`first rxjs ${res}`);
+    //interval
+    // const first$ = interval(100);
+    // first$.subscribe(res => {
+    //   console.log(`first rxjs ${res}`)
+    // })
+
+    const http$ = Observable.create(async observer => {
+      try {
+        const res = await axios(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        observer.next(res.data);
+        observer.complete();
+      } catch (er) {
+        observer.error(er);
+      }
     });
+
+    const posts$ = http$.pipe(
+      map(course => course.map(course => ({ ...course, name: "billy" })))
+    );
+
+    posts$.subscribe(
+      posts => console.log(posts),
+      noop,
+      () => {
+        console.log("completed");
+      }
+    );
   }, []);
 
   return (
